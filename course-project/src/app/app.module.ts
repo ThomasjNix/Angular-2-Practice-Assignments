44cacfbd7d934863f80f2c7559410b6d0a932ae6
Thomas Nix <tnix2@uncc.edu>
2017-09-01 10:18:49 -0400

    Added ingredient model as shared model and updated shopping list component and recipe component. New header styling for future navigation, basic styles added.

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -4,23 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
+
 import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 import { RecipeComponent } from './recipe/recipe.component';
+
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent,
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
-    RecipeComponent
+    RecipeComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
--- course-project/src/app/recipe/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Just Flour', 'It\'s literally just flour.', 'http://www.gretchensbakery.com/wp-content/uploads/2012/03/flour-1024x681.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
